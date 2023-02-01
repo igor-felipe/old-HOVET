dde5d10ec6fd95aee0d310b95859248238664cb2
Igor Felipe <54617151+igor-felipe@users.noreply.github.com>
2023-02-01 02:16:48 -0300

    fix: add helmet to improve security

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
+import helmet from "helmet";
 import { RegisterRoutes } from "../public/routes";
 
 const app: Application = express();
@@ -8,7 +9,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
-
+app.use(helmet());
 app.use(
   "/docs",
   swaggerUi.serve,
